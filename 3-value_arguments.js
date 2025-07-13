e268030844f65d26cf7880aa9a46d6136599e176
Gabtongues <aladebusayo.g@gmail.com>
2025-07-13 23:14:26 +0100

    4-concat.js

--- 3-value_arguments.js
+++ 3-value_arguments.js
@@ -1,7 +1,19 @@
-const arg = process.argv[2];
+// This script prints the first command-line argument passed to it.
+// If no arguments are provided, it prints "No argument".
 
-if (arg === undefined) {
-  console.log("No argument");
+// process.argv is an array containing the command-line arguments.
+// process.argv[0] is 'node' (the executable path).
+// process.argv[1] is the path to the script file.
+// process.argv[2] is the first actual argument provided by the user.
+
+const args = process.argv;
+
+// Check if the element at index 2 (the first user-provided argument) exists.
+// If it's not undefined, an argument was passed.
+if (args[2] !== undefined) {
+  // If an argument exists, print it to the console.
+  console.log(args[2]);
 } else {
-  console.log(arg);
+  // If no argument exists at index 2, print "No argument".
+  console.log("No argument");
 }
